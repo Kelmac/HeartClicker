ff2c134d07065650b957b7e2bae8eab662a2bef0
Piotr Teresiak <pteresiak@gmail.com>
2017-02-04 21:45:01 +0100

    create dummy GameplayScreen

---
+++ core/src/com/codor/game/heartclick/screens/GameplayScreen.java
@@ -0,0 +1,16 @@
+package com.codor.game.heartclick.screens;
+
+import com.codor.game.heartclick.HeartClickerGame;
+
+public class GameplayScreen extends AbstractScreen {
+
+	public GameplayScreen(HeartClickerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+
+	}
+
+}
