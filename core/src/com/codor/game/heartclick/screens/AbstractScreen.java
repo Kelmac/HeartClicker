833f380e168a4a968b48048d64072f27e5e840fc
Piotr Teresiak <pteresiak@gmail.com>
2017-02-04 21:12:32 +0100

    Create AbstracktScreen

---
+++ core/src/com/codor/game/heartclick/screens/AbstractScreen.java
@@ -0,0 +1,67 @@
+package com.codor.game.heartclick.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.codor.game.heartclick.HeartClickerGame;
+
+public abstract class AbstractScreen implements Screen {
+	protected HeartClickerGame game;
+	protected Stage stage;
+	private OrthographicCamera camera;
+	protected SpriteBatch spriteBatch;
+
+	public AbstractScreen(HeartClickerGame game) {
+		this.game = game;
+		createCamera();
+	}
+
+	private void createCamera() {
+		this.camera = new OrthographicCamera();
+		camera.setToOrtho(false, HeartClickerGame.WIDTH, HeartClickerGame.HEIGHT);
+		camera.update();
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		clearScreen();
+		camera.update();
+		spriteBatch.setProjectionMatrix(camera.combined);
+
+	}
+
+	private void clearScreen() {
+		Gdx.gl.glClearColor(0, 0, 0, 0.5f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void pause() {
+		game.setPaused(true);
+	}
+
+	@Override
+	public void resume() {
+		game.setPaused(false);
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		game.dispose();
+	}
+}
