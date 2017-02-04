a38bbd63e4a32a44b5f3e4ab0c2f0438f688ec9f
Piotr Teresiak <pteresiak@gmail.com>
2017-02-04 23:32:52 +0100

    add Player class

---
+++ core/src/com/codor/game/heartclick/entities/Player.java
@@ -0,0 +1,26 @@
+package com.codor.game.heartclick.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.codor.game.heartclick.HeartClickerGame;
+
+public class Player extends Image {
+
+	private final static Integer WIDTH = 110;
+	private final static Integer HEIGH = 90;
+
+	private final static Integer STARTING_X = HeartClickerGame.WIDTH / 2;
+	private final static Integer STARTING_Y = HeartClickerGame.HEIGHT;
+
+	public Player() {
+		super(new Texture("fullheart"));
+
+		this.setOrigin(WIDTH / 2, HEIGH / 2);
+		this.setSize(WIDTH, HEIGH);
+
+		// Starting position
+		this.setPosition(STARTING_X, STARTING_Y);
+
+	}
+
+}
