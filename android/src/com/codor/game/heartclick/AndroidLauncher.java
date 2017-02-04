725578b9c0dd5b90b5cd55f359e88f8fdb9e9dab
Piotr Teresiak <pteresiak@gmail.com>
2017-02-04 20:21:56 +0100

    init

---
+++ android/src/com/codor/game/heartclick/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.codor.game.heartclick;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.codor.game.heartclick.HeartClicker;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HeartClicker(), config);
+	}
+}
