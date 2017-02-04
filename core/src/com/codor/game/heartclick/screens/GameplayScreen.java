02505f02be323fc55e6d9fd019c2a371cd9d531b
Piotr Teresiak <pteresiak@gmail.com>
2017-02-04 23:48:27 +0100

    add Player to stage

--- core/src/com/codor/game/heartclick/screens/GameplayScreen.java
+++ core/src/com/codor/game/heartclick/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.codor.game.heartclick.screens;
 
 import com.codor.game.heartclick.HeartClickerGame;
+import com.codor.game.heartclick.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
+
+	private Player player;
 
 	public GameplayScreen(HeartClickerGame game) {
 		super(game);
@@ -10,7 +13,27 @@ public class GameplayScreen extends AbstractScreen {
 	}
 
 	private void init() {
+		initPlayer();
+	}
 
+	private void initPlayer() {
+		this.player = new Player();
+		stage.addActor(this.player);
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
+
+		spriteBatch.begin();
+		stage.draw();
+		spriteBatch.end();
+
+	}
+
+	private void update() {
+		stage.act();
 	}
 
 }
