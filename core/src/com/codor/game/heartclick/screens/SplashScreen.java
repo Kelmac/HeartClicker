9062bb556b2570b40897ae5be91c4f45c57ef0df
Piotr Teresiak <pteresiak@gmail.com>
2017-02-04 21:53:11 +0100

    change splashScreen to gameplayScreen after 2 sec

--- core/src/com/codor/game/heartclick/screens/SplashScreen.java
+++ core/src/com/codor/game/heartclick/screens/SplashScreen.java
@@ -1,15 +1,25 @@
 package com.codor.game.heartclick.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.codor.game.heartclick.HeartClickerGame;
 
 public class SplashScreen extends AbstractScreen {
 
 	private Texture splashImg;
 
-	public SplashScreen(HeartClickerGame game) {
+	public SplashScreen(final HeartClickerGame game) {
 		super(game);
 		init();
+
+		Timer.schedule(new Task() {
+
+			@Override
+			public void run() {
+				game.setScreen(new GameplayScreen(game));
+			}
+		}, 2.0f);
 	}
 
 	private void init() {
@@ -22,7 +32,7 @@ public class SplashScreen extends AbstractScreen {
 	public void render(float delta) {
 		super.render(delta);
 		spriteBatch.begin();
-		spriteBatch.draw(splashImg, HeartClickerGame.WIDTH/2, HeartClickerGame.HEIGHT/2);
+		spriteBatch.draw(splashImg, HeartClickerGame.WIDTH / 2, HeartClickerGame.HEIGHT / 2);
 		spriteBatch.end();
 
 	}
